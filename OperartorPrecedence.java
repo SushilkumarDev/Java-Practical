73b2525851361534768423b7f992a98cb8e74d45
SushiulkumarDev <sushilkumardeveloper@gmail.com>
2024-03-19 16:42:34 +0530

    code for Operartor Precedence

---
+++ OperartorPrecedence.java
@@ -0,0 +1,11 @@
+
+public class OperartorPrecedence {
+    public static void main(String[] args) {
+        int length = 15;
+        int breadth = 10;
+        
+        double perimeter = 2 * (length + breadth);
+        
+        System.out.println("The perimeter of the rectangle is: " + perimeter);
+    }
+}
