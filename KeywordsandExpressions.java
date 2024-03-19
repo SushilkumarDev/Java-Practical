b1ba58eb81f465ee0be1026092e1a3ee862f301e
SushiulkumarDev <sushilkumardeveloper@gmail.com>
2024-03-19 16:47:13 +0530

    Code for Keywords and Expressions

---
+++ KeywordsandExpressions.java
@@ -0,0 +1,19 @@
+
+public class KeywordsandExpressions {
+    public static void main(String[] args) {
+
+        double myValue = 10;
+        System.out.println(myValue);
+
+        int int5 = 10;
+        System.out.println("I am expression");
+
+        float area = 100 * 25;
+
+        if (area == 2500){
+
+              System.out.println("This is the area");
+        }
+    }
+}
+
---
+++ OperatorPrecedenceAssessment.java
@@ -0,0 +1,11 @@
+public class OperatorPrecedenceAssessment {
+    public static void main(String args[]) {
+        int length, breadth;
+        double perimeter = 0.0;
+        length = 10;
+        breadth = 5;
+        perimeter = (length + breadth)*2;
+        // perimeter = (length + breadth)* 2 ;
+        System.out.println("The perimeter of the rectangle is = " + perimeter);
+    }
+}
