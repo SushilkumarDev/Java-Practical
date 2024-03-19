0832dd578526ae9d08c0aeff8b124952f63f89ee
SushiulkumarDev <sushilkumardeveloper@gmail.com>
2024-03-19 15:48:04 +0530

    the frist practical of the course

---
+++ HelloWorldAssessment.java
@@ -0,0 +1,19 @@
+// Hello World!
+
+// Prepinsta is Awesome
+
+// Welcome to the Java course
+
+// If you want to learn Java in depth, complete this playlist.
+
+// Print the above lines as an output in java  by using System.out.println() method.
+
+
+public class HelloWorldAssessment{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("Prepinsta is Awesome");
+        System.out.println("Welcome to the Java Course");
+        System.out.println("If you want to learn java in depth, complete this playlist");
+    }
+}
