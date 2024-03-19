9d7a785470ef0b008b55851d6f4531929f75d1cb
SushiulkumarDev <sushilkumardeveloper@gmail.com>
2024-03-19 16:19:55 +0530

    the code for If then statements

---
+++ Ifthenstatements.java
@@ -0,0 +1,27 @@
+// This is a simple java program that checks if a person is eligible to vote based on their age
+
+public class Ifthenstatements{
+public static void main(String[] args) {
+    // Declare and initialize a boolean variable
+    boolean amIabovel8 = false;
+
+    // Check if the variable is true
+    if (amIabovel8 == true) {
+        System.out.println("You are eligible to vote");
+    }
+
+    // Check if the variable is false
+    if (amIabovel8 == false) {
+        System.out.println("You can't vote");
+    }
+
+    // Declare and initialize another boolean variable
+    boolean a = true;
+
+    // Check if the variable is true
+    if (a) {
+        System.out.println("A is true");
+        System.out.println("This is a test");
+    }
+}
+}
