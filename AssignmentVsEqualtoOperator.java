c09d5837dbaa54270d42bf1d25f4281eb24686b4
SushiulkumarDev <sushilkumardeveloper@gmail.com>
2024-03-19 16:38:09 +0530

    Code for Assignment Vs Equal to Operator is Successfully run

---
+++ AssignmentVsEqualtoOperator.java
@@ -0,0 +1,17 @@
+
+public class AssignmentVsEqualtoOperator {
+    public static void main(String[] args) {
+        int mobilePrice = 500;
+
+        if (mobilePrice == 4500 || mobilePrice >= 500) {
+            System.out.println("It's under my budget");
+
+            boolean isCold = false;
+
+            if (isCold == false) {
+                System.out.println("Yes, it is cold");
+            }
+        }
+    }
+}
+
