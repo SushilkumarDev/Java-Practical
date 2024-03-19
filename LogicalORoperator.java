fccd2d3c1ec58503d20b6a84b8801ac89c56e376
SushiulkumarDev <sushilkumardeveloper@gmail.com>
2024-03-19 16:24:47 +0530

    the Logical OR Operator code is run successfully

---
+++ LogicalORoperator.java
@@ -0,0 +1,20 @@
+
+public class LogicalORoperator{
+    public static void main(String[] args) {
+        
+        int c = 90;
+        int d = 100;
+    
+        if ((c >= 90) || (d < 100)) {
+        System.out.println("It's all good");
+        }
+    
+        boolean isItSunday = false;
+    
+         if (!isItSunday) {
+            System.out.println("It is not a Sunday");
+            }
+        }
+    }
+    
+    
