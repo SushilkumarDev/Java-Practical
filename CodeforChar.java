96357769ebe02d2391e67dfc0c6bcdfcd017c11c
SushiulkumarDev <sushilkumardeveloper@gmail.com>
2024-03-19 16:07:54 +0530

    code for char are succesfully learn

---
+++ CodeforChar.java
@@ -0,0 +1,11 @@
+public class CodeforChar {
+    public static void main(String[] args) {
+        char myChar = 'A';
+        System.out.println(myChar);
+        char myChar1 ='1';
+        System.out.println(myChar1);
+       
+       char myUniChar1 ='\u013f';
+       System.out.println(myUniChar1);
+    }
+}
