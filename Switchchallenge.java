09de0b87fb62386cb925a8244499c2b6e7f22749
SushiulkumarDev <sushilkumardeveloper@gmail.com>
2024-03-20 22:09:50 +0530

    Code for Switch challenge was successfully complete

---
+++ Switchchallenge.java
@@ -0,0 +1,31 @@
+public class Switchchallenge {
+
+    public static void main(String[] args) {
+        char myLetter = 'p';
+
+        switch (Character.toUpperCase(myLetter)) {
+            case 'A':
+                System.out.println("It is a vowel");
+                break;
+
+            case 'E':
+                System.out.println("It is a vowel");
+                break;
+
+            case 'I':
+                System.out.println("It is a vowel");
+                break;
+            case 'O':
+                System.out.println("It is a vowel");
+                break;
+
+            case 'U':
+                System.out.println("It is a vowel");
+                break;
+
+            default:
+                System.out.println("Oops! Not a Vowel");
+        }
+    }
+}
+
