21f8c76c7efa8ff8ac001b42370d9f9b6a3f2153
SushiulkumarDev <sushilkumardeveloper@gmail.com>
2024-03-21 19:31:56 +0530

    Solved Code for While Do-while loop part 1

---
+++ WhileDowhilepart1.java
@@ -0,0 +1,30 @@
+public class   WhileDowhilepart1 {                        
+    public static void main(String[] args) {
+        int count = 1;
+
+        
+        // while(count != 10){
+        //     System.out.println("The Count is :" + count);
+        //     count ++;
+        // }
+        
+
+        // for (count = 1; count <= 10; count++) {
+        //     System.out.println("The Count is :" + count);
+        // }
+        
+        
+        // while (true) {
+        //     if (count == 6) {
+        //     break;
+        //     }
+        // System.out.println("The Count is: " + count);
+        // count++;
+        // }
+        
+          do {
+             System.out.println("The Count is: " + count);
+             count++;
+            } while (count != 6);
+    }
+}
